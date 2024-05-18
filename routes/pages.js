4208acb12b80c7548482bb7e817ce88535388ffb
MasiAI1 <mr.fire205@gmail.com>
2024-05-18 20:17:13 +0700

    Закончили работу с jwt

--- routes/pages.js
+++ routes/pages.js
@@ -3,7 +3,7 @@ const { sendIndex } = require("../controllers/auth.js");
 const {sendDashboard} = require("../controllers/auth");
 const {checkCookiesJWT, checkAuth} = require("../middlewares/auth");
 
-pagesRouter.get("/", sendIndex);
 pagesRouter.get("/admin/**",checkCookiesJWT,checkAuth, sendDashboard);
+pagesRouter.get("/", sendIndex);
 
 module.exports = pagesRouter
